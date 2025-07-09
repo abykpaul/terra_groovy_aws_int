8124b2796e542777c612340add5a9407492b02bf
abykpaul <44528042+abykpaul@users.noreply.github.com>
2025-07-09 20:30:07 +0530

    Delete aws-free-tier-project/terraform/dev/terraform {   backend "s3" {     bucket         = "jack-terraform-tfstate-bucket"     key            = "dev directory

--- aws-free-tier-project/terraform/dev/terraform {   backend "s3" {     bucket         = "jack-terraform-tfstate-bucket"     key            = "dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "abykpaul-terraform-devops"
-    key            = "dev/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-  }
-}
