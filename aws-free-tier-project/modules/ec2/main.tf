01e63529db58aabbd27c65a3c6cb1f2403d856e4
abykpaul <44528042+abykpaul@users.noreply.github.com>
2025-07-10 02:17:09 +0530

    Update main.tf

--- aws-free-tier-project/modules/ec2/main.tf
+++ aws-free-tier-project/modules/ec2/main.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "web" {
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
   key_name               = var.key_name
+  associate_public_ip_address = true
 
   user_data = <<-EOF
               #!/bin/bash
