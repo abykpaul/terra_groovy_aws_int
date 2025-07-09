ed57da81b1491b5ad370810903e9703c91fdfb80
abykpaul <44528042+abykpaul@users.noreply.github.com>
2025-07-09 20:31:07 +0530

    Update backend.tf

--- aws-free-tier-project/terraform/dev/backend.tf
+++ aws-free-tier-project/terraform/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "jack-terraform-tfstate-bucket"
+    bucket         = "abykpaul-terraform-devops"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
   }
