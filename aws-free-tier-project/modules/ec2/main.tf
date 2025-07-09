471c930a20ea4457028da72f94d06ab310051b80
abykpaul <44528042+abykpaul@users.noreply.github.com>
2025-07-10 01:13:38 +0530

    Update main.tf

--- aws-free-tier-project/modules/ec2/main.tf
+++ aws-free-tier-project/modules/ec2/main.tf
@@ -5,6 +5,14 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = [var.security_group_id]
   key_name               = var.key_name
 
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install -y httpd
+              sudo systemctl start httpd
+              sudo systemctl enable httpd
+              echo "Welcome to ${var.environment} server!" > /var/www/html/index.html
+              EOF
   tags = {
     Name        = "${var.name}-ec2"
     Environment = var.environment
